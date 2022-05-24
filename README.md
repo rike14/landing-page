d0b185d5a50324c3e0d8374942cf32b66ce83b61
rike14 <henriquemk00@gmail.com>
2022-05-24 11:46:16 -0300

    add new project landing-page

---
+++ README.md
@@ -0,0 +1,123 @@
+# Leia em:  -- <a href="#portugues">Português </a> / <span id="portugues">PT</span> -- • -- Read in: -- <a href="#english">English</a> / EN --
+
+<h1 align="center">
+    <img alt="HenriqueDev" title="#HenriqueDev" src="./src/Henrique2.gif" width="90%"/>
+</h1>
+
+<p align="center">
+ <a href="#-sobre-o-projeto">Sobre</a> •
+ <a href="#-funcionalidades">Funcionalidades</a> •
+ <a href="#-tecnologias">Tecnologias</a> •
+ <a href="#-autor">Autor</a> •
+</p>
+
+## 💻 Sobre o projeto
+
+💻 Landing Page - Veja essa landing page. Espero que goste!! :pushpin: :wink:
+
+---
+
+## ⚙️ Funcionalidades
+
+- [x] Início:
+    <h1 align="center">
+        <img alt="Home" title="#home" src="./src/home.gif" width="90%"/>
+    </h1>
+
+- [x] Produtos vindo da API:
+    <h1 align="center">
+        <img alt="products of api" title="#products" src="./src/products.gif" width="90%"/>
+    </h1>
+
+- [x] Usuário se registrando para receber promoções:
+    <h1 align="center">
+        <img alt="form of user" title="#form1" src="./src/form1.gif" width="90%"/>
+    </h1>
+
+- [x] Enviando dados de um amigo:
+    <h1 align="center">
+        <img alt="form of friend" title="#form2" src="./src/form2.gif" width="90%"/>
+    </h1>
+
+---
+
+## 🛠 Tecnologias
+
+A seguinte ferramenta foi usada na construção do projeto:
+
+- **[HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)**
+- **[CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)**
+- **[JavaScript](https://www.javascript.com/)**
+
+## 🦸 Autor
+
+ <img style="border-radius: 50%;" src="https://media-exp1.licdn.com/dms/image/C4E03AQHEAO7lZFv_DQ/profile-displayphoto-shrink_100_100/0/1628044108879?e=2147483647&v=beta&t=THXLaSYL1EF43H1OCCCJVn2m-Cz2HY535lqkyam17B8" width="100px;" alt=""/>
+ <br />
+ <sub><b>Henrique M. Kronhardt</b></sub></a>
+ <br />
+
+[![Linkedin Badge](https://img.shields.io/badge/-Henrique_Kronhardt-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/henriquekronhardt/)](https://www.linkedin.com/in/henriquekronhardt/)
+
+---
+
+Feito com ❤️ por Henrique M. Kronhardt 👋🏽 [Entre em contato!](https://www.linkedin.com/in/henriquekronhardt/)
+
+# Read in: -- <a href="#english">English</a> / <span id="english">EN</span> -- • -- Leia em:  -- <a href="#portugues">Português </a> / PT --
+
+<p align="center">
+ <a href="#-about-the-project">About</a> •
+ <a href="#-features">Features</a> •
+ <a href="#-technologies">Technologies</a> •
+ <a href="#-author">Author</a> •
+</p>
+
+## 💻 About the project
+
+💻 Landing Page - Check out this landing page, hope you like it!! :pushpin: :wink:
+
+---
+
+## ⚙️ Features
+
+- [x] Home:
+    <h1 align="center">
+        <img alt="Home" title="#home" src="./src/home.gif" width="90%"/>
+    </h1>
+
+- [x] Products from API:
+    <h1 align="center">
+        <img alt="products of api" title="#products" src="./src/products.gif" width="90%"/>
+    </h1>
+
+- [x] User register for receive promotions:
+    <h1 align="center">
+       <img alt="form of user" title="#form1" src="./src/form1.gif" width="90%"/>
+    </h1>
+
+- [x] The user send data for friend:
+    <h1 align="center">
+        <img alt="form of friend" title="#form2" src="./src/form2.gif" width="90%"/>
+    </h1>
+
+---
+
+## 🛠 Technologies
+
+The following tool was used in building the project:
+
+- **[HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)**
+- **[CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)**
+- **[JavaScript](https://www.javascript.com/)**
+
+## Author
+
+ <img style="border-radius: 50%;" src="https://media-exp1.licdn.com/dms/image/C4E03AQHEAO7lZFv_DQ/profile-displayphoto-shrink_100_100/0/1628044108879?e=2147483647&v=beta&t=THXLaSYL1EF43H1OCCCJVn2m-Cz2HY535lqkyam17B8" width="100px;" alt=""/>
+ <br />
+ <sub><b>Henrique M. Kronhardt</b></sub></a>
+ <br />
+
+[![Linkedin Badge](https://img.shields.io/badge/-Henrique_Kronhardt-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/henriquekronhardt/)](https://www.linkedin.com/in/henriquekronhardt/)
+
+---
+
+Made with ❤️ by Henrique M. Kronhardt 👋🏽 [Get in touch!](https://www.linkedin.com/in/henriquekronhardt/)
---
+++ main.js
@@ -0,0 +1,147 @@
+const formSendinformationsUser = document.getElementById('formSendinformationsUser')
+const formSendFriendData = document.getElementById('formSendFriendData')
+const cardProducts = document.getElementById('products')
+function toastSnackBar() {
+    let snackbar = document.getElementById("snackbar");
+    snackbar.className = "show";
+    setTimeout(function () { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+}
+
+formSendinformationsUser.onsubmit = function (event){
+    event.preventDefault()
+
+    let haveError = false
+
+    const inputFirstNameUser = document.getElementById('firstname')
+    const inputLastNameUser = document.getElementById('lastname')
+    const inputEmailUser = document.getElementById('email')
+    const inputCpfUser = document.getElementById('cpf')
+
+
+
+    if (!inputFirstNameUser.value){
+        haveError = true
+        inputFirstNameUser.classList.add('inputError')
+        let spanError = inputFirstNameUser.nextSibling.nextSibling 
+        spanError.innerText = `Please first name cannot null!`
+    }else {
+        inputFirstNameUser.classList.remove('inputError')
+        let spanError = inputFirstNameUser.nextSibling.nextSibling 
+        spanError.innerText = ''
+    }
+    if (!inputLastNameUser.value) {
+        haveError = true
+        inputLastNameUser.classList.add('inputError')
+        let spanError = inputLastNameUser.nextSibling.nextSibling
+        spanError.innerText = `Please last name cannot null!`
+    } else {
+        inputLastNameUser.classList.remove('inputError')
+        let spanError = inputLastNameUser.nextSibling.nextSibling
+        spanError.innerText = ''
+    }
+    if(!inputEmailUser.value){
+        haveError = true
+        inputEmailUser.classList.add('inputError')
+        let spanError = inputEmailUser.nextSibling.nextSibling
+        spanError.innerText = `Please e-mail cannot null!`
+    }else{
+        inputEmailUser.classList.remove('inputError')
+        let spanError = inputEmailUser.nextSibling.nextSibling
+        spanError.innerText = ''
+    }
+    if (!inputCpfUser.value) {
+        haveError = true
+        inputCpfUser.classList.add('inputError')
+        let spanError = inputCpfUser.nextSibling.nextSibling
+        spanError.innerText = `Please CPF cannot null!`
+        
+    }else {
+        inputCpfUser.classList.remove('inputError')
+        let spanError = inputCpfUser.nextSibling.nextSibling
+        spanError.innerText = ''
+    }
+
+    if(!haveError){
+        toastSnackBar()
+        inputFirstNameUser.value = ''
+        inputLastNameUser.value = ''
+        inputEmailUser.value = ''
+        inputCpfUser. value = ''
+    }
+
+}
+
+formSendFriendData.onsubmit = function(event){
+    event.preventDefault()
+
+    let haveError = false
+
+    const inputNameFriend = document.getElementById('inputNameFriend')
+    const inputEmailFriend = document.getElementById('inputEmailFriend')
+    
+    if(!inputNameFriend.value){
+        haveError = true
+        inputNameFriend.classList.add('inputError')
+        let spanError = inputNameFriend.nextSibling.nextSibling
+        spanError.innerText = 'Please name of your friend cannot null!'
+    } else {
+        inputNameFriend.classList.remove('inputError')
+        let spanError = inputNameFriend.nextSibling.nextSibling
+        spanError.innerText = ''
+    }
+    if (!inputEmailFriend.value){
+        haveError = true
+        inputEmailFriend.classList.add('inputError')
+        let spanError = inputEmailFriend.nextSibling.nextSibling
+        spanError.innerText = 'Please e-mail of your friend cannot null!'
+    }else {
+        inputEmailFriend.classList.remove('inputError')
+        let spanError = inputEmailFriend.nextSibling.nextSibling
+        spanError.innerText = ''
+    }
+    if(!haveError){
+        toastSnackBar()
+        inputNameFriend.value = ''
+        inputEmailFriend.value = ''
+    }
+}
+
+
+function responseJSON(response){
+    return response.json()
+}
+
+function listProducts(data){
+    const products = data.products
+
+    products.forEach(element => {
+        cardProducts.innerHTML += `
+        <div class="card">
+          <img src="${element.image}" style="width:70%">
+          <h3>${element.name}</h3>
+          <br>
+          <p>${element.description}</p>
+          <br>
+          <p class="price">From <s>$${element.oldPrice}</s></p>
+          <p class="price boldPrice">to <p class="newPrice boldPrice">$${element.price}</p></p>
+          <p class="price ">Or <p class="boldPrice">${element.installments.count}x</p> of <p class="newPrice boldPrice">$${element.installments.value.toFixed(2)}</p></p>
+          <p><button>Buy</button></p>
+        </div>
+        `
+    })
+
+}
+
+function error(){
+    console.log('Oh no! Erro sorry.')
+}
+
+let page = 0
+async function callAPI_Products(){
+    page++
+    const data = await fetch(`https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${page}`)
+                     .then(responseJSON)
+                     .then(listProducts)
+                     .catch(error)
+}
+callAPI_Products()
